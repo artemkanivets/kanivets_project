cd8f701c0c93f990e43bd5650343c520b6297b0b
Admin <artemkanivecc@gmail.com>
2023-09-25 15:08:02 +0300

    lab_2

---
+++ lab_2_kanivets.py
@@ -0,0 +1,14 @@
+#Task1
+print("Programming***Essentials***in...Python")
+#Task2
+print('     *\n    * *\n   *   *\n  *     *\n ***   ***\n   *   *\n   *   *\n   *****')
+#Task3
+print("I'm student")
+#Task4
+print('\"I\'m\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"')
+#Task5
+decimal=int('500', 8)
+print(decimal)
+#Task6
+decimal1=int('777', 16)
+print(decimal1)
